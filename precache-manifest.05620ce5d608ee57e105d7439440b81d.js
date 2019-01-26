e8da27a8216bd8c2622cd87a1b2bb3f483d1244d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-26 11:18:27 +0000

    Deploy ErSachinVashist/deploytest to github.com/ErSachinVashist/deploytest.git:gh-pages

---
+++ precache-manifest.05620ce5d608ee57e105d7439440b81d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3bd02234a980d7ae993",
+    "url": "/deploytest/static/js/main.e3bd0223.chunk.js"
+  },
+  {
+    "revision": "6ebd15ca75ca0a06fa89",
+    "url": "/deploytest/static/js/1.6ebd15ca.chunk.js"
+  },
+  {
+    "revision": "81fc1f3e29da66f480e4",
+    "url": "/deploytest/static/js/runtime~main.81fc1f3e.js"
+  },
+  {
+    "revision": "97cb69449251ff28c86b5eb3671a65a3",
+    "url": "/deploytest/index.html"
+  }
+];
